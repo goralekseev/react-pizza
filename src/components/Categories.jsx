511ed63f294a43fd2953c33091e8b7fa3c3bb520
Igor Alekseev <tauron19@gmail.com>
2022-05-27 00:59:12 -0400

    array of pizzas with dynamic values

---
+++ src/components/Categories.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const setIndex = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className='categories'>
+      <ul>
+        {/* <li onClick={() => {setIndex(0);}} className={activeIndex === 0 ? "active" : ""}> Все</li> */}
+        {categories.map((value, index) => (
+          <li
+            onClick={() => {
+              setIndex(index);
+            }}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
